c27b8474bf450c862c45496f6c4b6ae80840e586
JiniZacharias <jinizachariasadappacheril2210@gmail.com>
2024-12-05 20:36:02 -0500

    updating app.py

--- app.py
+++ app.py
@@ -56,4 +56,6 @@ def ui():
     return app.send_static_file('dashboard.html')
 
 if __name__ == '__main__':
+    import logging
+    logging.basicConfig(level=logging.DEBUG)
     app.run(host='0.0.0.0', port=80)
